8874a705a5f7c877bc68c9ab4543ec36a97acf8f
LaksCastro <lakhsmycastro336@gmail.com>
2020-08-09 22:49:26 -0300

    add 'are you sure' question on try exit app

--- lib/app/screens/home/home_screen.dart
+++ lib/app/screens/home/home_screen.dart
@@ -14,6 +14,46 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final homeStore = getIt<HomeStore>();
 
+  Future<bool> preventAcidentalExit() async {
+    bool request = false;
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Tem certeza que deseja sair?"),
+          content: Text(
+            "Nem mais um episódio? É só 20 minutinhos!",
+            style: TextStyle(height: 1.5),
+          ),
+          actions: [
+            FlatButton(
+              color: Theme.of(context).colorScheme.secondaryVariant,
+              child: Text("Sim",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSecondary)),
+              onPressed: () {
+                request = true;
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text("Não",
+                  style: TextStyle(
+                      color: Theme.of(context).textTheme.bodyText1.color)),
+              onPressed: () {
+                request = false;
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    return request;
+  }
+
   @override
   void dispose() {
     homeStore.dispose();
@@ -23,59 +63,65 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        initialIndex: 0,
-        length: MainTabs.tabs.length,
-        child: Scaffold(
-            appBar: AppBar(
-              title: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Anime Dart",
-                        style: TextStyle(
-                            color: Theme.of(context)
-                                .primaryTextTheme
-                                .bodyText1
-                                .color
-                                .withOpacity(.60))),
-                    GestureDetector(
-                        onTap: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (_) => AboutScreen()));
-                        },
-                        child: Container(
-                            alignment: Alignment.centerRight,
-                            height: 100,
-                            width: 100,
-                            child: Icon(OMIcons.code,
+    return WillPopScope(
+        child: DefaultTabController(
+            initialIndex: 0,
+            length: MainTabs.tabs.length,
+            child: Scaffold(
+                appBar: AppBar(
+                  title: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Anime Dart",
+                            style: TextStyle(
                                 color: Theme.of(context)
                                     .primaryTextTheme
                                     .bodyText1
                                     .color
-                                    .withOpacity(.3))))
-                  ]),
-              automaticallyImplyLeading: false,
-              bottom: TabBar(
-                indicatorColor: Colors.white,
-                isScrollable: true,
-                tabs: [
-                  for (final tab in MainTabs.tabs)
-                    Container(
-                        alignment: Alignment.center,
-                        width: 100,
-                        child: Tab(text: tab.label))
-                ],
-              ),
-            ),
-            body: TabBarView(
-              children: [for (final tab in MainTabs.tabs) tab.builder(context)],
-            ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => SearchScreen()));
-                },
-                child: Icon(Icons.search))));
+                                    .withOpacity(.60))),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => AboutScreen()));
+                            },
+                            child: Container(
+                                alignment: Alignment.centerRight,
+                                height: 100,
+                                width: 100,
+                                child: Icon(OMIcons.code,
+                                    color: Theme.of(context)
+                                        .primaryTextTheme
+                                        .bodyText1
+                                        .color
+                                        .withOpacity(.3))))
+                      ]),
+                  automaticallyImplyLeading: false,
+                  bottom: TabBar(
+                    indicatorColor: Colors.white,
+                    isScrollable: true,
+                    tabs: [
+                      for (final tab in MainTabs.tabs)
+                        Container(
+                            alignment: Alignment.center,
+                            width: 100,
+                            child: Tab(text: tab.label))
+                    ],
+                  ),
+                ),
+                body: TabBarView(
+                  children: [
+                    for (final tab in MainTabs.tabs) tab.builder(context)
+                  ],
+                ),
+                floatingActionButton: FloatingActionButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => SearchScreen()));
+                    },
+                    child: Icon(Icons.search)))),
+        onWillPop: preventAcidentalExit);
   }
 }
